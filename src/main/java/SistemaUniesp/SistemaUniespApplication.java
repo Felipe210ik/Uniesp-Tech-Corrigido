00a0134d8160e40593af919925a390acc7506a21
Felipe Fernandes <felipe360cf@gmail.com>
2026-03-09 18:15:26 -0300

    Novo Branch

---
+++ src/main/java/SistemaUniesp/SistemaUniespApplication.java
@@ -0,0 +1,13 @@
+package SistemaUniesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUniespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUniespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SistemaUniesp/SistemaUniespApplicationTests.java
@@ -0,0 +1,13 @@
+package SistemaUniesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUniespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
